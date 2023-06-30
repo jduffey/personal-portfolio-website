431904ed3ecb06a9314b1f948e3346b447f9eb7b
Jed Duffey <jedduffey@gmail.com>
2023-06-30 15:42:49 -0400

    Minor styling on nav bar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -3,14 +3,12 @@ import Link from "next/link";
 export default function NavBar({ children, navLinks }) {
     return (
         <section>
-            <nav
-                className="navbar navbar-expand-lg navbar-light bg-light bg-orange-500 text-lg text-white"
-            >
-                <Link className='m-6' href="/">Home</Link>
+            <nav className="bg-orange-400 text-lg text-white">
+                <Link className='m-6 hover:text-gray-800' href="/">Home</Link>
                 {navLinks.map((child) => {
                     return (
                         <Link
-                            className="ml-2 mr-2 pl-2 pr-2 border-2 border-white rounded-md"
+                            className="m-4 pl-2 pr-2 hover:text-gray-800"
                             href={child.route}
                         >
                             {child.name}
