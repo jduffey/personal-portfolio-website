034ecb0469be3e7e62d48d9c99fb0bb95f62d40a
Jed Duffey <jedduffey@gmail.com>
2023-06-30 12:50:48 -0400

    Make list of ul elements for links to pages

--- app/page.js
+++ app/page.js
@@ -13,8 +13,8 @@ export default function Home() {
             <div className='mb-4 text-2xl text-gray-300'>
                 {"Code, etc."}
             </div>
-            <div style={{ display: 'flex' }}>
-                <div className='flex text-center m-0 mb-4 p-2 border-2 border-gray-300 rounded-3xl'>
+            <div className='flex'>
+                <div className='flex text-center m-0 mb-4 p-2 border-2 border-gray-300 rounded-2xl'>
                     {[
                         { icon: FaGithub, url: githubUrl },
                         { icon: FaTwitter, url: twitterUrl },
@@ -22,7 +22,7 @@ export default function Home() {
                     ].map((item, index) => (
                         <a
                             key={index}
-                            className='no-underline font-bold m-2 text-gray-300 hover:text-purple-500'
+                            className='no-underline font-bold m-2 text-gray-300 hover:text-amber-500'
                             href={item.url}
                             target="_blank"
                             rel="noreferrer"
@@ -30,6 +30,67 @@ export default function Home() {
                             <item.icon size={36} />
                         </a>
                     ))}
+                </div>
+            </div>
+            <div className='flex flex-col text-gray-300'>
+                <div className='mb-2 text-xl font-bold'>
+                    {"Projects + Prototypes"}
+                </div>
+                <div className='mb-4'>
+                    <ul>
+                        {[
+                            { text: "Mark I", url: "mark-i" },
+                            { text: "Mark II", url: "mark-ii" },
+                            { text: "Mark IV", url: "mark-iv" },
+                        ].map((item, index) => (
+                            <li key={index}>
+                                <a
+                                    className='ml-2 hover:text-amber-500'
+                                    href={item.url}>
+                                    {item.text}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className='mb-2 text-xl font-bold'>
+                    {"Games"}
+                </div>
+                <div className='mb-4'>
+                    <ul>
+                        {[
+                            { text: "Roulette", url: "roulette" },
+                            { text: "Three Card Poker", url: "three-card-poker" },
+                            { text: "Space Poker", url: "space-poker" },
+                        ].map((item, index) => (
+                            <li key={index}>
+                                <a
+                                    className='ml-2 hover:text-amber-500'
+                                    href={item.url}>
+                                    {item.text}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className='mb-2 text-xl font-bold'>
+                    {"Data Viz + Simulations"}
+                </div>
+                <div className='mb-4'>
+                    <ul>
+                        {[
+                            { text: "Secretary Problem", url: "secretary-problem" },
+                            { text: "Capture-Recapture", url: "capture-recapture" },
+                        ].map((item, index) => (
+                            <li key={index}>
+                                <a
+                                    className='ml-2 hover:text-amber-500'
+                                    href={item.url}>
+                                    {item.text}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         </main>
