cd746976b5e12e8667c6a6737e157a75cdcba84f
Jed Duffey <jedduffey@gmail.com>
2023-07-01 09:19:10 -0400

    Draft of playing cards (all diamonds A-K)

--- app/(games)/space-poker/page.js
+++ app/(games)/space-poker/page.js
@@ -4,6 +4,73 @@ export default function SpacePoker() {
     return (
         <>
             Space Poker
+            <section className="flex flex-row">
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">A</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">2</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">3</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">4</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">5</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">6</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">7</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">8</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">9</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">T</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">J</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">Q</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 100 150">
+                    <rect x="0" y="0" width="100" height="150" style={{ fill: "white", stroke: "black", strokeWidth: 2 }} />
+                    <text x="30" y="60" font-family="Arial" font-size="60" fill="red">K</text>
+                    <polygon points="50,70 70,100 50,130 30,100" style={{ fill: "red", stroke: "red", strokeWidth: 2 }} />
+                </svg>
+            </section>
             <MountainRange />
         </>
     )
