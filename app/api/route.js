6789c80575d920d47c4c05a6b6be522d481fb194
Jed Duffey <RP_Node>
2023-07-12 13:25:15 -0400

    Begin replacing route.js with base fee API calls

--- app/api/route.js
+++ app/api/route.js
@@ -1,16 +1,84 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
+
+    const url = "http://192.168.0.10:8545";
+
+    const getBlockNumber = async () => {
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Cache-Control': 'no-store',
+                },
+                body: JSON.stringify({
+                    method: "eth_blockNumber",
+                    params: [],
+                    jsonrpc: "2.0",
+                    id: 1,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Network reponse error: ${response.status} ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            console.log(data);
+
+            const blockNumberHex = data.result;
+            const blockNumber = parseInt(blockNumberHex, 16);
+
+            return blockNumberHex;
+        }
+        catch (error) {
+            console.error(error);
+        }
+    };
+
+    const getBlockByNumber = async (blockNumber) => {
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Cache-Control': 'no-store',
+                },
+                body: JSON.stringify({
+                    method: "eth_getBlockByNumber",
+                    params: [blockNumber, true],
+                    jsonrpc: "2.0",
+                    id: 1,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Network reponse error: ${response.status} ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            // console.log(data);
+
+            return data.result;
+        }
+        catch (error) {
+            console.error(error);
+        }
+    };
+
+    const blockNumber = await getBlockNumber();
+    console.log(`Block number: ${blockNumber}`);
+
+    const block = await getBlockByNumber(blockNumber);
+    console.log(`Block: ${block}`);
+
+    const baseFee = block.baseFeePerGas;
+
     return NextResponse.json(
         {
-            greeting: "Hello!",
-            message: "This is a demo of the Next.js API route.",
-            favorites: {
-                food: "Pizza",
-                number: "1559",
-                color: "Space Black",
-                baseballTeam: "Reds",
-            }
+            blockNumber,
+            baseFee,
         }
     )
 }
